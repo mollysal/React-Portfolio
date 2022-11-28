4700d802bf4012a0812c593b2fff9aaedd8f4d6f
Molly Salmonsen <salmonsen.molly@gmail.com>
2022-11-27 20:05:24 -0500

    Resume page working with link

--- portfolio/src/pages/ResumePage.jsx
+++ portfolio/src/pages/ResumePage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import '../style/ResumePage.css';
+import { BsFillCloudDownloadFill } from "react-icons/bs";
+
 
 export const ResumePage = () => {
-    return <section>
-        <h1>Hello World! This is ResumePage!</h1>
-    </section>
+    return <div className='row m-4'>
+        <div className="col resumeDownload">
+                <a href='https://drive.google.com/file/d/1mMaGIvWInDrAs59LEdz6SIIoLl7OVBM2/view?usp=share_link' target="_blank" className='linkText' download="SalmonsenResume">Download my Resume Here:  <BsFillCloudDownloadFill /></a>
+
+        </div>
+    </div>
 }
