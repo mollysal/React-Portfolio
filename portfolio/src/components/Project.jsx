8c0469b391ebf6a0e132534c43a3233fa18a97a3
Molly Salmonsen <salmonsen.molly@gmail.com>
2022-11-28 23:03:26 -0500

    Rendering images

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -4,8 +4,8 @@ import '../style/Portfolio.css';
 export const Project = ({ url, title, github, description, image }) => {
     return <div className="card py-6" style={{width: '18rem'}}>
         <div className="card-body">
-            {/* Need to figure out why images not rendering */}
-            <img src={image} className='card-img-top' />
+            {/* Need to have require for images to rendering */}
+            <img src={require('../images/'+ image )} className='card-img-top' />
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <a href={url} className="card-link">Deployed Project</a>
--- portfolio/src/utils/projects.js
+++ portfolio/src/utils/projects.js
@@ -2,42 +2,42 @@ export const projects = [
     {
         title: "RapidE!",
         description: "E-Commerce Site",
-        image: "../images/RapidE!.png",
+        image: "RapidE.png",
         url: "https://rapid-e.herokuapp.com/",
         github: "https://github.com/mollysal/RapidE"
     },
     {
         title: "Note Taker",
         description: "Note Taker with Express.js",
-        image: "../images/NoteTaker.png",
+        image: "NoteTaker.png",
         url: "https://mollysalnotetaker.herokuapp.com/",
         github: "https://github.com/mollysal/Note-Taker"
     },
     {
         title: "Space & Weather Tracker",
         description: "Check for local space launches & the weather nearby.",
-        image: "../images/Space&Weather.png",
+        image: "Space&Weather.png",
         url: "https://mollysal.github.io/Space-Weather/",
         github: "https://github.com/mollysal/Space-Weather"
     },
     {
         title: "Coding Quiz",
         description: "Take the quiz to check your knowledge.",
-        image: "../images/CodingQuiz.png",
+        image: "CodingQuiz.png",
         url: "https://mollysal.github.io/Web-API-Coding-Quiz/",
         github: "https://github.com/mollysal/Web-API-Coding-Quiz"
     },
     {
         title: "J.A.T.E. - PWA Text Editor",
         description: "Text Editor that can run offline",
-        image: "../images/JATE_Installed.png",
+        image: "JATE_Installed.png",
         url: "https://jate-ms.herokuapp.com/",
         github: "https://github.com/mollysal/PWA-Text-Editor"
     },
     {
         title: "Coming Soon Project 3",
         description: "Final Project",
-        image: "../images/BalloonStock.jpg",
+        image: "BalloonStock.jpg",
         url: "https://github.com/mollysal/",
         github: "https://github.com/mollysal/"
     }
